814991ac2639662885e05895c12d6d2424be05be
Ali Bulut <ali@alibulut.net>
2021-07-04 17:45:13 +0300

    migration files added

---
+++ SampleNLayerProject.Data/Migrations/20210704143713_add_count_to_category.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SampleNLayerProject.Data.Migrations
+{
+    public partial class add_count_to_category : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Count",
+                table: "Categories",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Count",
+                table: "Categories");
+        }
+    }
+}
---
+++ SampleNLayerProject.Data/Migrations/20210704144345_remove_count_from_category.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SampleNLayerProject.Data.Migrations
+{
+    public partial class remove_count_from_category : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Count",
+                table: "Categories");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Count",
+                table: "Categories",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
