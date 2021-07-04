6b83b7ebdcf529a4b02eb803163bfdac71ec9889
Ali Bulut <ali@alibulut.net>
2021-07-04 13:38:20 +0300

    adding EF packages and creating DbContext class

---
+++ SampleNLayerProject.Data/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using SampleNLayerProject.Core.Models;
+
+namespace SampleNLayerProject.Data
+{
+    // AppDbContext refers db.
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        // defining db tables
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+
+        // method that works before table creation operation on db.
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+}
