fe9579e87fa216362944b8b532af361b024dee66
Ali Bulut <ali@alibulut.net>
2021-07-04 18:34:33 +0300

    CategoryService class added

---
+++ SampleNLayerProject.Service/Services/CategoryService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using SampleNLayerProject.Core.Models;
+using SampleNLayerProject.Core.Repositories;
+using SampleNLayerProject.Core.Services;
+using SampleNLayerProject.Core.UnitOfWorks;
+
+namespace SampleNLayerProject.Service.Services
+{
+    public class CategoryService : Service<Category>, ICategoryService
+    {
+        public CategoryService(IUnitOfWork unitOfWork, IRepository<Category> repository) : base(unitOfWork, repository)
+        {
+        }
+
+        public async Task<Category> GetWithProductsByIdAsync(int categoryId)
+        {
+            return await _unitOfWork.Categories.GetWithProductsByIdAsync(categoryId);
+        }
+    }
+}
