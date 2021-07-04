5009b20610bfbf96744e38c936f4f43324c11d36
Ali Bulut <ali@alibulut.net>
2021-07-04 16:54:41 +0300

    configuring Startup.cs file in API project

--- SampleNLayerProject.API/Startup.cs
+++ SampleNLayerProject.API/Startup.cs
@@ -6,10 +6,14 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using SampleNLayerProject.Core.UnitOfWorks;
+using SampleNLayerProject.Data;
+using SampleNLayerProject.Data.UnitOfWorks;
 
 namespace SampleNLayerProject.API
 {
@@ -25,6 +29,17 @@ namespace SampleNLayerProject.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration["ConnectionStrings:SqlConStr"].ToString());
+            });
+
+            // when a request comes up and it sees IUnitOfWork in somewhere of the project,
+            // it'll create a new instance of UnitOfWork and will use it instead of IUnitOfWork.
+            // If in one request there is more than one IUnitOfWork, it will use same instance for those also.
+            // If we want to create new instance for every new IUnitOfWork, we need to use AddTransient instead of AddScoped.
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+
             services.AddControllers();
         }
 
