2968030d699680e31af4874efd99a019ce92f9ae
Ali Bulut <ali@alibulut.net>
2021-07-04 16:15:24 +0300

    rename appDbContext field in ProductRepository

--- SampleNLayerProject.Data/Repositories/ProductRepository.cs
+++ SampleNLayerProject.Data/Repositories/ProductRepository.cs
@@ -11,14 +11,14 @@ namespace SampleNLayerProject.Data.Repositories
         // _context => coming from Repository class protected field.
         // _context field is converted to AppDbContext. Because we'll use AppDbContext class as context.
         // so we'll be able to reach tables defined in AppDbContext class.
-        private AppDbContext appDbContext { get => _context as AppDbContext; }
+        private AppDbContext _appDbContext { get => _context as AppDbContext; }
         public ProductRepository(DbContext context) : base(context)
         {
         }
 
         public async Task<Product> GetWithCategoryByIdAsync(int productId)
         {
-            return await appDbContext.Products.Include(x => x.Category).SingleOrDefaultAsync(x => x.Id == productId);
+            return await _appDbContext.Products.Include(x => x.Category).SingleOrDefaultAsync(x => x.Id == productId);
         }
     }
 }
