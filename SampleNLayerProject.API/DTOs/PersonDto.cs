616b14aa5828e778d735e27348d2516f297e5557
Ali Bulut <ali@alibulut.net>
2021-07-05 19:43:08 +0300

    PersonDto added

---
+++ SampleNLayerProject.API/DTOs/PersonDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace SampleNLayerProject.API.DTOs
+{
+    public class PersonDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+    }
+}
