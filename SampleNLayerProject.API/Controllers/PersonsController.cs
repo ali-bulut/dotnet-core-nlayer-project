ec8fdf6a8d0461790f2ad38d432c7e2556c60654
Ali Bulut <ali@alibulut.net>
2021-07-05 19:43:28 +0300

    PersonsController has been created with endpoints

---
+++ SampleNLayerProject.API/Controllers/PersonsController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using SampleNLayerProject.API.DTOs;
+using SampleNLayerProject.Core.Models;
+using SampleNLayerProject.Core.Services;
+
+namespace SampleNLayerProject.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PersonsController : ControllerBase
+    {
+        private readonly IService<Person> _service;
+        private readonly IMapper _mapper;
+
+        public PersonsController(IService<Person> service, IMapper mapper)
+        {
+            _service = service;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var persons = await _service.GetAllAsync();
+            return Ok(_mapper.Map<IEnumerable<PersonDto>>(persons));
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var person = await _service.GetByIdAsync(id);
+            return Ok(_mapper.Map<PersonDto>(person));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Save(PersonDto personDto)
+        {
+            var newPerson = await _service.AddAsync(_mapper.Map<Person>(personDto));
+            return Created(string.Empty, _mapper.Map<PersonDto>(newPerson));
+        }
+
+        [HttpPut]
+        public IActionResult Update(PersonDto personDto)
+        {
+            _service.Update(_mapper.Map<Person>(personDto));
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Remove(int id)
+        {
+            var person = _service.GetByIdAsync(id).Result;
+            _service.Remove(person);
+            return NoContent();
+        }
+    }
+}
