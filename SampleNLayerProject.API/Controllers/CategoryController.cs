8ca6d194a713d4497497a932e28c9a750860e93a
Ali Bulut <ali@alibulut.net>
2021-07-05 18:19:22 +0300

    adding automapper to category controller

--- SampleNLayerProject.API/Controllers/CategoryController.cs
+++ SampleNLayerProject.API/Controllers/CategoryController.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using SampleNLayerProject.API.DTOs;
 using SampleNLayerProject.Core.Services;
 
 namespace SampleNLayerProject.API.Controllers
@@ -11,18 +13,20 @@ namespace SampleNLayerProject.API.Controllers
     public class CategoryController : Controller
     {
         private readonly ICategoryService _categoryService;
+        private readonly IMapper _mapper;
 
         // it will fill by using dependency injection that is configured in startup.cs.
-        public CategoryController(ICategoryService categoryService)
+        public CategoryController(ICategoryService categoryService, IMapper mapper)
         {
             _categoryService = categoryService;
+            _mapper = mapper;
         }
 
         [HttpGet]
         public async Task<IActionResult> GetAll()
         {
             var categories = await _categoryService.GetAllAsync();
-            return Ok(categories);
+            return Ok(_mapper.Map<IEnumerable<CategoryDto>>(categories));
         }
     }
 }
