b8929ab1c397fb5bccafff2bbe20cb9ca71e135e
Ali Bulut <ali@alibulut.net>
2021-07-05 19:36:33 +0300

    Person model and migrations added

---
+++ SampleNLayerProject.Data/Configurations/PersonConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SampleNLayerProject.Core.Models;
+
+namespace SampleNLayerProject.Data.Configurations
+{
+    public class PersonConfiguration : IEntityTypeConfiguration<Person>
+    {
+        public void Configure(EntityTypeBuilder<Person> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).UseIdentityColumn();
+
+            builder.Property(x => x.Name).IsRequired().HasMaxLength(50);
+            builder.Property(x => x.Surname).IsRequired().HasMaxLength(50);
+
+            builder.ToTable("Persons");
+        }
+    }
+}
