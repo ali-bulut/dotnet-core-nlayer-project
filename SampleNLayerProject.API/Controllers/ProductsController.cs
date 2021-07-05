11ae0f792907662b1b9d2d977306583d6a4b9d43
Ali Bulut <ali@alibulut.net>
2021-07-05 19:18:41 +0300

    ProductController has been created with endpoints

---
+++ SampleNLayerProject.API/Controllers/ProductsController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using SampleNLayerProject.API.DTOs;
+using SampleNLayerProject.Core.Models;
+using SampleNLayerProject.Core.Services;
+
+namespace SampleNLayerProject.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductService _productService;
+        private readonly IMapper _mapper;
+
+        // it will fill by using dependency injection that is configured in startup.cs.
+        public ProductsController(IProductService productService, IMapper mapper)
+        {
+            _productService = productService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var products = await _productService.GetAllAsync();
+            return Ok(_mapper.Map<IEnumerable<ProductDto>>(products));
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var product = await _productService.GetByIdAsync(id);
+            return Ok(_mapper.Map<ProductDto>(product));
+        }
+
+        [HttpGet("{id}/category")]
+        public async Task<IActionResult> GetWithCategoryById(int id)
+        {
+            var productWithCategory = await _productService.GetWithCategoryByIdAsync(id);
+            return Ok(_mapper.Map<ProductWithCategoryDto>(productWithCategory));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Save(ProductDto productDto)
+        {
+            var newProduct = await _productService.AddAsync(_mapper.Map<Product>(productDto));
+            return Created(string.Empty, _mapper.Map<ProductDto>(newProduct));
+        }
+
+        [HttpPut]
+        public IActionResult Update(ProductDto productDto)
+        {
+            _productService.Update(_mapper.Map<Product>(productDto));
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Remove(int id)
+        {
+            var product = _productService.GetByIdAsync(id).Result;
+            _productService.Remove(product);
+            return NoContent();
+        }
+    }
+}
